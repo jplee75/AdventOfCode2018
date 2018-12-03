e12db22a20a84aebe889c9777eb7b8707c7dc7af
jplee75@gmail.com <jplee75@gmail.com>
2018-12-03 12:26:48 -0600

    cleanup

--- Day1/Part2/main.go
+++ Day1/Part2/main.go
@@ -26,33 +26,18 @@ func main() {
 
 	var finalCount int
 	var loop int
-	var frequencies []int
 	var match = false
 	dupFrequencies := map[int]bool{0: true}
 
 	for !match {
 		for _, x := range inputData {
 			finalCount += x
-			frequencies = append(frequencies, finalCount)
 			if _, ok := dupFrequencies[finalCount]; ok {
 				fmt.Printf("The duplier is : %v\n", finalCount)
 				match = true
 				break
 			}
 			dupFrequencies[finalCount] = true
-			//fmt.Println(finalCount)
-			//sort.Ints(frequencies)
-			//y := sort.SearchInts(frequencies, finalCount)
-			//if loop >= 1 && y != len(frequencies) {
-			//	if finalCount == frequencies[y+1] {
-			//		match = true
-			//		break
-			//	} else {
-			//		frequencies = append(frequencies, finalCount)
-			//	}
-			//} else {
-			//
-			//}
 		}
 		loop += 1
 		fmt.Println(loop)
